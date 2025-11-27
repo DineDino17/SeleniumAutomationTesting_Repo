7c83a8a38953a8b3567352c33e5a850f4d996a33
admin <admin@DESKTOP-42NORKR>
2025-11-27 18:35:04 +0530

    Updated

--- Selenium-Automation-Project/src/test/java/runners/TextBoxTestRunner.java
+++ Selenium-Automation-Project/src/test/java/runners/TextBoxTestRunner.java
@@ -4,11 +4,15 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions( features= {"src/test/java/features/TextBoxTestinginTestLeaf.feature"},
+@CucumberOptions( features= {"./src/test/resources/features/TextBoxTestinginTestLeaf.feature"},
 					glue="stepDefinitions",
 					dryRun = !true,
 					monochrome=true,
-					plugin= {"pretty","html:target/cucumber-report.html","json:target/cucumber-report.json"})
+					publish = false,
+					plugin= {"pretty","html:target/cucumber-report.html","json:target/cucumber-report.json"},
+					tags = ("@Test,"
+							+ "@Test1")
+)
 public class TextBoxTestRunner extends AbstractTestNGCucumberTests{
 
 }
